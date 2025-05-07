3787f2257fee3f3bd6276ab2e20646286c8882c8
niconoel <nicolasnoek@gmail.com>
2025-05-07 16:24:44 +0200

    fix game - safe version

--- scripts/platform.gd
+++ scripts/platform.gd
@@ -1,15 +1,11 @@
 extends StaticBody2D
 
-@onready var labelContent = $Label
-var listObjects = []
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	modulate = Color(Color.MEDIUM_PURPLE, 0.7)
-	labelContent.text = str(listObjects)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	#visible = global.is_dragging
-	visible = true
-	
+	visible = global.is_dragging
