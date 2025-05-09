785e3de67bfeebecaeb7536b82a2885b5797343f
Benjamin Corbelet <118047639+BenCorb@users.noreply.github.com>
2025-05-09 13:41:12 +0200

    Script to manage stats UI elements

---
+++ scripts/game_ui.gd
@@ -0,0 +1,16 @@
+extends CanvasLayer
+
+
+@onready var union_gauge: AnimatedSprite2D = $Union/UnionGauge
+@onready var happy_gauge: AnimatedSprite2D = $Happy/HappyGauge
+@onready var money_label: Label = $Node/MoneyLabel
+
+var union_level = 0
+var happy_level = 0
+var money = 0
+
+
+func _process(delta: float) -> void:
+	union_gauge.frame = int(union_level / 10)
+	happy_gauge.frame = int(happy_level / 10)
+	money_label.text = "Money " + str(money)
